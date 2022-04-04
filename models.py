3118c0848dd66191876551cf6dbeaa96c759c0db
Harsh Bhamore <73632348+harsh-bhamore@users.noreply.github.com>
2022-04-05 00:55:09 +0530

    create all pms models

--- models.py
+++ models.py
@@ -73,3 +73,39 @@ class DayCountTracker(models.Model):
     image_count = models.IntegerField(default=0)
     image_count_authored = models.IntegerField(default=0)
 
+class BufferImages(models.Model):
+    '''
+    DB Class to handle Buffer / Extra Images for a Project.
+    '''
+    parent_project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    project_name = models.CharField(max_length=100, blank=False, null=False)
+    client_poc = models.CharField(max_length=1000, blank=False, null=False)
+    client_poc_email = models.EmailField(max_length=1000, blank=False, null=False)
+    delivery_owner = models.ForeignKey(DeliveryOwner, on_delete=models.CASCADE)
+    project_type = models.ForeignKey(ProjectType, on_delete=models.CASCADE)
+    date_booked = models.DateField()
+    doc_type = models.ForeignKey(DocType, on_delete=models.CASCADE)
+    estimated_date_of_delivery = models.DateField()
+    image_count = models.IntegerField(default=0)
+    status = models.CharField(max_length=100, blank=False, null=False)
+    team = models.CharField(max_length=100, blank=False, null=False)
+    image_count_authored = models.IntegerField(default=0)
+    date_delivered = models.DateField()
+
+class Finance(models.Model):
+    '''
+    DB Class to handle all the Project Finances.
+    '''
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project_quote = models.FloatField(default=0.0)
+    project_currency = models.CharField(max_length=100, blank=False, null=False)
+    expected_invoicing_date = models.DateField()
+    po_amount = models.FloatField(default=0.0)
+    po_number = models.CharField(max_length=1000, blank=False, null=False)
+    date_invoiced = models.DateField()
+    amount_invoiced = models.FloatField(default=0.0)
+    number_of_days_since_invoiced = models.IntegerField(default=0)
+    expected_money_in_date = models.DateField()
+    money_in = models.FloatField(default=0.0)
+
